440cb8af71659f3e254b06107449c2f254fb4edf
Laurenz-Guevara <laurenzguevara@outlook.com>
2023-10-22 19:38:06 +0100

    Added slash commands

--- main.go
+++ main.go
@@ -1,74 +1,161 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"os"
 	"os/signal"
-	"strings"
-	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
-const prefix string = "!c"
+var (
+	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutting down or not")
+)
 
-func main() {
-	fmt.Println("Starting Gatsby Cat Bot")
-	godotenv.Load()
-	token := os.Getenv("BOT_TOKEN")
-	sess, err := discordgo.New("Bot " + token)
-	if err != nil {
-		log.Fatal(err)
+var s *discordgo.Session
+
+func init() { flag.Parse() }
+
+func init() {
+	// Load environment variables from .env file
+	env := godotenv.Load()
+	if env != nil {
+		log.Fatalf("Error loading .env file: %v", env)
 	}
 
-	commandResponses := map[string]string{
-		"Cat":  "meow",
-		"Dog":  "bark",
-		"bird": "tweet",
+	// Set BotToken from environment variable
+	botToken := os.Getenv("BOT_TOKEN")
+
+	// Initialize Discord session
+	var initErr error
+	s, initErr = discordgo.New("Bot " + botToken)
+	if initErr != nil {
+		log.Fatalf("Invalid bot parameters: %v", initErr)
+	}
+}
+
+var (
+	integerOptionMinValue          = 1.0
+	dmPermission                   = false
+	defaultMemberPermissions int64 = discordgo.PermissionManageServer
+
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name: "mood",
+			Description: "Work out if Gatsby is happy, sad or something else.",
+		},
+		{
+			Name:        "pet",
+			Description: "Subcommands and command groups example",
+			Options: []*discordgo.ApplicationCommandOption{
+				// {
+				// 	Name:        "subcommand-group",
+				// 	Description: "Subcommands group",
+				// 	Options: []*discordgo.ApplicationCommandOption{
+				// 		{
+				// 			Name:        "nested-subcommand",
+				// 			Description: "Nested subcommand",
+				// 			Type:        discordgo.ApplicationCommandOptionSubCommand,
+				// 		},
+				// 	},
+				// 	Type: discordgo.ApplicationCommandOptionSubCommandGroup,
+				// },
+				{
+					Name:        "gatsby",
+					Description: "Give gatsby a head pat",
+					Type:        discordgo.ApplicationCommandOptionSubCommand,
+				},
+			},
+		},
 	}
 
-	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"mood": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Gatsby's mood is cheerful!",
+				},
+			})
+		},
+		"pet": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			options := i.ApplicationCommandData().Options
+			content := ""
 
-		//Map
-		if response, exists := commandResponses[m.Content]; exists {
-			s.ChannelMessageSend(m.ChannelID, response)
-		} 
-
-		args := strings.Split(m.Content, " ")
-
-		if args[0] != prefix {
-			return
-		}
-
-		input := strings.ToLower(args[1])
-
-		if input == "commands" {
-			for key := range commandResponses {
-				s.ChannelMessageSend(m.ChannelID, key)
+			// As you can see, names of subcommands (nested, top-level)
+			// and subcommand groups are provided through the arguments.
+			switch options[0].Name {
+			case "gatsby":
+				content = "meow *You petted Gatsby! +1 Rep*"
+				
+			// case "subcommand-group":
+			// 	options = options[0].Options
+			// 	switch options[0].Name {
+			// 	case "nested-subcommand":
+			// 		content = "Nice, now you know how to execute nested commands too"
+			// 	default:
+			// 		content = "Oops, something went wrong.\n" +
+			// 			"Hol' up, you aren't supposed to see this message."
+			// 	}
 			}
-		}
 
-		if input == "owner" {
-			s.ChannelMessageSend(m.ChannelID, "Zushi and Cookiee are my owners.")
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: content,
+				},
+			})
+		},
+	}
+)
+
+func init() {
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
 		}
 	})
- 
-	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+}
 
-	err = sess.Open()
+func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+	err := s.Open()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Cannot open the session: %v", err)
 	}
-	defer sess.Close()
 
-	fmt.Println("The bot is online!")
-	
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
+	log.Println("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	defer s.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	if *RemoveCommands {
+		log.Println("Removing commands...")
+
+		for _, v := range registeredCommands {
+			err := s.ApplicationCommandDelete(s.State.User.ID, *GuildID, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
+	log.Println("Gracefully shutting down.")
 }
